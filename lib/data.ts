3648ed6dbe600bcc87a235a556965eb4a591236a
QasimKhan146 <qasimkhan1243@gmail.com>
2025-05-16 11:38:15 +0430

    home carousel

--- lib/data.ts
+++ lib/data.ts
@@ -33,6 +33,29 @@ const data = {
       href: '/page/help',
     },
   ],
+  carousels: [
+    {
+      title: 'most popular Shows For Sale',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner1.jpg',
+      url: '/search?category=Shoes',
+      isPublished: true,
+    },
+    {
+      title: 'Best Seller in t-Shirts',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner2.jpg',
+      url: '/search?category=T-Shirt',
+      isPublished: true,
+    },
+    {
+      title: 'Best Deals on Wrist Watches',
+      buttonCaption: 'See More',
+      image: '/images/banner3.jpg',
+      url: '/search?category=Wrist Watches',
+      isPublished: true,
+    },
+  ],
 }
 
 export default data
